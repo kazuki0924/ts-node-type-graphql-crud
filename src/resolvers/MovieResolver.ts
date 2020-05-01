b0fb7b837b6379a28ea415af052394421eaff01f
Kazuki Matsuzaki <kazuki_matsuzaki@mates-system.com>
2020-05-01 17:15:56 +0900

    Update MovieResolver.ts

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -1,15 +1,19 @@
-import { Resolver, Mutation, Arg, Int } from 'type-graphql'
+import { Resolver, Mutation, Arg, Int, Query } from 'type-graphql'
 import { Movie } from '../entity/Movie'
-
 @Resolver()
 export class MovieResolver {
   @Mutation(() => Boolean)
   async createMovie(
-    @Arg('title') title: string
+    @Arg('title') title: string,
     // because number is not a GraphQL types, you must inform GraphQL what kind of number it is
     @Arg('minutes', () => Int) minutes: number
   ) {
-    await Movie.insert({title, minutes})
+    await Movie.insert({ title, minutes })
     return true
+  }
+
+  @Query(() =>)
+  movies() {
+    return Movie.find()
   }
 }
